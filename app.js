faf12dbf90b14dbf9e6a831c4bc50fcdc23d03de
Ashif Khan <ashifkhn02@gmail.com>
2021-09-20 10:48:12 +0530

    done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var btnTranslate=document.querySelector("#btn-translate")
 var textInput=document.querySelector("#text-input")
 var outputDiv=document.querySelector("#output")
 // var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverURL="https://api.funtranslations.com/translate/valyrian.json"
+var serverURL="https://api.funtranslations.com/translate/doge.json"
 
 
 
